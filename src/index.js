781204dc534aaef2cea7d4114f80d204ca103494
Nikolay Nemshilov <nemshilov@gmail.com>
2017-10-25 08:39:59 +1100

    Making the eslint happy too

--- src/index.js
+++ src/index.js
@@ -4,13 +4,11 @@ import humanize from './humanizer';
 const EMPTY_SCHEMA = { type: 'object' };
 
 export default class Validator {
-  customValidator = () => null;
-
   constructor(schema = EMPTY_SCHEMA) {
     this.update({ schema });
   }
 
-  update({ validate, schema }) {
+  update({ schema }) {
     if (schema) {
       this.schema = schema;
       this.valid = new Ajv({ allErrors: true }).compile(schema);
